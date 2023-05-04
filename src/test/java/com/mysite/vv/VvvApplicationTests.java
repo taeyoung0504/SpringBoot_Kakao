25e3f6d4f4194c7c2d836e3243f0395166421dfe
taeyoung0504 <taeyoung050405@gmail.com>
2023-05-04 18:42:55 +0900

    kakao login

---
+++ src/test/java/com/mysite/vv/VvvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.vv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
